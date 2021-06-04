1b2881ae0549b64c2651967ec7ab63ecbcf8f90e
steffanciprian <steffanciprian@gmail.com>
2021-06-04 16:31:59 +0300

    am facut pagina de preturi trebuie corectat in josul paginii cu flexDirection row si justifyContent spacebetween dar inca nu merge trebuie inceputa pagina de localizare

--- src/index.js
+++ src/index.js
@@ -8,14 +8,37 @@ import {Route, Switch} from "react-router";
 import PreturiScreen from "./screens/PreturiScreen";
 import NavbarComponent from "./components/NavbarComponent";
 import {Routes} from "./Routes";
+import {FacebookLoginButton, InstagramLoginButton} from "react-social-login-buttons";
 
 ReactDOM.render(
     <BrowserRouter>
         <NavbarComponent routes={Routes}/>
         <Switch>
-                <Route exact path="/" component={App}/>
-                <Route exact path="/preturi" component={PreturiScreen}/>
-            </Switch>
+            <Route exact path="/" component={App}/>
+            <Route exact path="/preturi" component={PreturiScreen}/>
+        </Switch>
+        <div className="alTreileaDiv">
+            <div className="divPolitica">
+                <h3>Politica de confidentialitate</h3>
+                <h3>Politica de cookie</h3>
+                <FacebookLoginButton text='' style={{width: 30, height: 30}} iconColor='grey' iconSize={15}/>
+                <InstagramLoginButton text='' style={{width: 30, height: 30}} iconColor='grey' iconSize={15}/>
+            </div>
+            <div className="divContactProgram">
+                <div>
+                    <h2 className="h">CONTACT</h2>
+                    <h4 className="h">Zona Piata Dorobanti:</h4>
+                    <h4 className="h">Radu Beller 25</h4>
+                    <h4 className="h"> Secotor 1, Bucuresti</h4>
+                    <h4 className="h"> Programri: 0730 665 468</h4>
+                </div>
+                <div><h2 className="h">PROGRAM</h2>
+                    <h4 className="h">Luni-Vineri</h4>
+                    <h4 className="h">11 AM - 9 PM</h4>
+                    <h4 className="h"> Sambata</h4></div>
+                <h4 className="h"> 9:30 AM - 6:30 PM</h4>
+            </div>
+        </div>
     </BrowserRouter>,
     document.getElementById('root')
 );
--- src/screens/PreturiScreen.js
+++ src/screens/PreturiScreen.js
@@ -2,7 +2,6 @@ import React from "react";
 import {withRouter} from "react-router";
 import "./PreturiScreen.css"
 import DetaliiPreturi from "../components/DetaliiPreturi";
-import {FacebookLoginButton, InstagramLoginButton} from "react-social-login-buttons";
 
 const PreturiScreen = () => {
     return (
@@ -26,28 +25,7 @@ const PreturiScreen = () => {
                 <DetaliiPreturi serviciu="Tuns copil" subserviciu=" tuns/spalat" pret="60 lei"/>
                 <DetaliiPreturi serviciu="Abonament 2 X Frizerie" subserviciu=" tuns/spalat" pret="80 lei"/>
             </div>
-            <div className="alTreileaDiv">
-                <div className="divPolitica">
-                    <h3>Politica de confidentialitate</h3>
-                    <h3>Politica de cookie</h3>
-                    <FacebookLoginButton text='' style={{width: 30, height: 30}} iconColor='grey' iconSize={15}/>
-                    <InstagramLoginButton text='' style={{width: 30, height: 30}} iconColor='grey' iconSize={15}/>
-                </div>
-                <div className="divContactProgram">
-                    <div>
-                        <h2 className="h">CONTACT</h2>
-                        <h4 className="h">Zona Piata Dorobanti:</h4>
-                        <h4 className="h">Radu Beller 25</h4>
-                        <h4 className="h"> Secotor 1, Bucuresti</h4>
-                        <h4 className="h"> Programri: 0730 665 468</h4>
-                    </div>
-                    <div><h2 className="h">PROGRAM</h2>
-                        <h4 className="h">Luni-Vineri</h4>
-                        <h4 className="h">11 AM - 9 PM</h4>
-                        <h4 className="h"> Sambata</h4></div>
-                    <h4 className="h"> 9:30 AM - 6:30 PM</h4>
-                </div>
-            </div>
+
         </div>
     )
 }
