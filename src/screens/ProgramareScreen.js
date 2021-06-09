447879454f2997436d5ea5171be7260652bc94ab
steffanciprian <steffanciprian@gmail.com>
2021-06-09 16:41:15 +0300

    1. am facut pozitionarea in pagina programare 2. am reusit sa schimb designul pentru item-ul selectat

--- src/screens/ProgramareScreen.js
+++ src/screens/ProgramareScreen.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 import './ProgramareScreen.css'
 
 const ProgramareScreen = () => {
-    const [servicii, setServicii] = useState([]),
+    let [servicii, setServicii] = useState([]),
         [nameStep1, setNameStep1] = useState('Servicii'),
         [nameStep2, setNameStep2] = useState('Stylist'),
         [nameStep3, setNameStep3] = useState('Date'),
@@ -15,6 +15,7 @@ const ProgramareScreen = () => {
         [nameStep5, setNameStep5] = useState('Confirm'),
         [nameServiciu, setNameServiciu] = useState(''),
         [selectedId, setSelectedId] = useState(-1),
+        [isLoaded, setIsLoaded] = useState(false),
         [id, setId] = useState(-1);
 
     //fetch din backend pentru recuperare de servicii
@@ -26,122 +27,133 @@ const ProgramareScreen = () => {
 
             console.log(result)
             setServicii(result.data);
+            setIsLoaded(true)
         }
         getServiciiFromBackend();
 
     }, []);
 
-    const setIds = (serviciu) => {
+    let setIds = (serviciu) => {
         setId(serviciu.id)
         setSelectedId(serviciu.id)
+        console.log(selectedId)
     }
 
 
     //functie pentru renderuit serviciile
-    const renderServiciu = serviciu => {
+    const renderServiciu = (serviciu) => {
         return (
-            <div className= {selectedId === serviciu.id ? 'container-selected' :'container-for-each-serviciu'}
-                  onClick={() => setIds(serviciu)}>
+            <div key={serviciu.id}
+                 className={selectedId === serviciu.id ? 'container-selected' : 'container-for-each-serviciu'}
+                 onClick={() => setIds(serviciu)}>
                 <p style={{margin: 10, fontSize: 15}}>{serviciu.name}</p>
-                <p style={{margin: 10, fontSize: 15}}>{serviciu.pret}</p>
+                <p style={{margin: 10, fontSize: 15}}>{serviciu.pret} {serviciu.moneda}</p>
             </div>
 
         );
     }
 
     // setup the step content
-    const step1Content = <h1>Step 1 Content</h1>;
-    const step2Content = <h1>Step 2 Content</h1>;
-    const step3Content = <h1>Step 3 Content</h1>;
-    const step4Content = <h1>Step 4 Content</h1>;
-    const step5Content = <h1>Step 5 Content</h1>;
+    const step1Content =
+        <div className='flat-list-container'>
+            <FlatList
+                list={servicii}
+                renderItem={renderServiciu}
+                renderWhenEmpty={() => <div>List is empty!</div>}
+                sortBy={["name", {key: "name", descending: true}]}
+                // groupBy={serviciu => serviciu > 18 ? 'Over 18' : 'Under 18'}
+            />
+        </div>;
+    const step2Content = <div><p>2</p></div>;
+    const step3Content = <div><p>3</p></div>;
+    const step4Content = <div><p>4</p></div>;
+    const step5Content = <div><p>5</p></div>;
 
     // setup step validators, will be called before proceeding to the next step
     function step2Validator() {
         // return a boolean
-        console.log(nameServiciu)
-        setNameStep1(nameServiciu)
-        console.log(nameServiciu)
-
+        console.log(selectedId)
         return true;
     }
 
     function step3Validator() {
         // return a boolean
-        setNameStep1(nameServiciu)
         return true;
     }
 
     function step4Validator() {
         // return a boolean
+        return true;
+
     }
 
     function step5Validator() {
         // return a boolean
+        return true;
+
     }
 
     function onFormSubmit() {
         // handle the submit logic here
         // This function will be executed at the last step
         // when the submit button (next button in the previous steps) is pressed
+        return true;
+
     }
 
     return (
         <div className='container-entire-page'>
-            <div className='container-progress-bar'>
-                <StepProgressBar
-                    startingStep={0}
-                    onSubmit={onFormSubmit}
-                    steps={[
-                        {
-                            label: nameStep1,
-                            // subtitle: '20%',
-                            // name: 'step 1',
-                            // content: step1Content
-                            validator: step2Validator
-                        },
-                        {
-                            label: nameStep2,
-                            // subtitle: '40%',
-                            // name: 'step 2',
-                            // content: step2Content,
-                            validator: step2Validator
-                        },
-                        {
-                            label: nameStep3,
-                            // subtitle: '60%',
-                            // name: 'step 3',
-                            // content: step3Content,
-                            validator: step3Validator
-                        },
-                        {
-                            label: nameStep4,
-                            // subtitle: '80%',
-                            // name: 'step 3',
-                            // content: step4Content,
-                            validator: step4Validator
-                        },
-                        {
-                            label: nameStep5,
-                            // subtitle: '100%',
-                            // name: 'step 3',
-                            // content: step5Content,
-                            validator: step5Validator
-                        }
-                    ]}
-                />
-            </div>
+            {isLoaded ?
+                <div className='container-progress-bar'>
 
+                    <StepProgressBar
+                        wrapperClass='content-style'
+                        buttonWrapperClass='button-wrapper-class'
+                        startingStep={0}
+                        onSubmit={onFormSubmit}
+                        steps={[
+                            {
+                                label: nameStep1,
+                                // subtitle: '20%',
+                                // name: 'step 1',
+                                content: step1Content,
+                                validator: step2Validator
+                            },
+                            {
+                                label: nameStep2,
+                                // subtitle: '40%',
+                                // name: 'step 2',
+                                content: step2Content,
+                                validator: step2Validator
+                            },
+                            {
+                                label: nameStep3,
+                                // subtitle: '60%',
+                                // name: 'step 3',
+                                content: step3Content,
+                                validator: step3Validator
+                            },
+                            {
+                                label: nameStep4,
+                                // subtitle: '80%',
+                                // name: 'step 3',
+                                content: step4Content,
+                                validator: step4Validator
+                            },
+                            {
+                                label: nameStep5,
+                                // subtitle: '100%',
+                                // name: 'step 3',
+                                content: step5Content,
+                                validator: step5Validator
+                            }
+                        ]}
+                    />
+                </div>
+                :
+                <p>loading...</p>
+            }
 
-            <div className='flat-list-container'>
-                <FlatList
-                    list={servicii}
-                    renderItem={renderServiciu}
-                    renderWhenEmpty={() => <div>List is empty!</div>}
-                    sortBy={["name", {key: "name", descending: true}]}
-                    // groupBy={serviciu => serviciu > 18 ? 'Over 18' : 'Under 18'}
-                />
-            </div>
 
         </div>
     )
